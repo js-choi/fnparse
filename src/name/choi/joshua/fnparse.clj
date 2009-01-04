91b1cd254e0434c84bf5f5455f77b034d1f87f32
Joshua Choi <joshua@choi.name>
2009-01-03 20:33:51 -0700

    Committer: Joshua Choi <joshua@choi.name>
     On branch master
     Changes to be committed:
    	new file:   src/name/choi/joshua/fnparse.clj
    	new file:   src/name/choi/joshua/fnparse/json.clj
    	new file:   test/test_parse.clj
    	new file:   test/test_parse_json.clj

---
+++ src/name/choi/joshua/fnparse.clj
@@ -0,0 +1,178 @@
+(ns name.choi.joshua.fnparse)
+
+; A rule is a function that:
+; - Takes a collection of tokens.
+; - If the token sequence is valid, it returns a vector containing the (0) consumed symbols'
+;   products and (1) a sequence of the remaining symbols or nil. In all documentation here,
+;   "a rule's products" is the first element of a valid result from the rule.
+; - If the given token sequence is invalid and the rule fails, it simply returns nil.
+
+(defn term
+  "Creates a rule function that is a terminal rule of the given validator--that is, it accepts
+  only tokens for whom (validator token) is true.
+  (def a (term validator)) would be equivalent to the EBNF
+    a = ? (validator %) evaluates to true ?;
+  The new rule's product would be the first token, if it fulfills the validator.
+  If the token does not fulfill the validator, the new rule simply returns nil."
+  [validator]
+  (fn [tokens]
+    (let [first-token (first tokens)]
+         (if (validator first-token),
+             [first-token (rest tokens)]))))
+
+(defn semantics
+  "Creates a rule function from attaching a semantic hook function to the given subrule--that
+  is, its products are from applying the semantic hook to the subrule's products. When the
+  subrule fails and returns nil, the new rule will return nil."
+  [subrule semantic-hook]
+  (fn [tokens]
+      (let [subrule-result (subrule tokens)]
+           (if (not (nil? subrule-result))
+               [(semantic-hook (subrule-result 0)) (subrule-result 1)]))))
+
+(defn constant-semantics
+  "Creates a rule function from attaching a constant semantic hook function to the given
+  subrule--that is, its product is a constant value. When the subrule fails and returns nil,
+  the new rule will return nil."
+  [subrule semantic-value]
+  (semantics subrule (fn [_] semantic-value)))
+
+(defn lit
+  "Creates a rule function that is the terminal rule of the given literal token--that is, it
+  accepts only tokens that are equal to the given literal token.
+  (def a (lit \"...\")) would be equivalent to the EBNF
+    a = \"...\";
+  The new rule's product would be the first token, if it equals the given literal token.
+  If the token does not equal the given literal token, the new rule simply returns nil."
+  [literal-token]
+  (term #(= % literal-token)))
+
+(defn re-term
+  "Creates a rule function that is the terminal rule of the given regex--that is, it accepts
+  only tokens that match the given regex.
+  (def a (re-term #\"...\")) would be equivalent to the EBNF
+    a = ? (re-matches #\"...\" %) evaluates to true ?;
+  The new rule's product would be the first token, if it matches the given regex.
+  If the token does not match the given regex, the new rule simply returns nil."
+  [token-regex]
+  (term #(re-matches token-regex %)))
+
+(defn conc
+  "Creates a rule function that is the concatenation of the given subrules--that is, each
+  subrule followed by the next.
+  (def a (conc b c d)) would be equivalent to the EBNF
+    a = b, c, d;
+  The new rule's products would be the vector [b-product c-product d-product]. If any of
+  the subrules don't match in the right place, the new rule simply returns nil."
+  [& subrules]
+  (fn [tokens]
+      (loop [products [], token-queue tokens, rule-queue subrules]
+            (if (nil? rule-queue),
+                [products token-queue],
+                (let [curr-result ((first rule-queue) token-queue)]
+                     (if (not (nil? curr-result))
+                         (recur (conj products (curr-result 0))
+                               (curr-result 1)
+                               (rest rule-queue))))))))
+
+(defn alt
+  "Creates a rule function that is the alternative of the given subrules--that is, any one
+  of the given subrules. Note that the subrules' order matters: the very first rule that
+  accepts the given tokens will be selected.
+  (def a (alt b c d)) would be equivalent to the EBNF
+    a = b | c | d;
+  The new rule's product would be b-product, c-product, or d-product depending on which
+  of the rules first accepts the given tokens. If none of the subrules matches, the new rule
+  simply returns nil."
+  [& subrules]
+  (fn [tokens]
+      (some #(% tokens) subrules)))
+
+(defn opt
+  "Creates a rule function that is the optional form of the given subrule--that is, either
+  the presence of the absence of the subrule.
+  (def a (opt b)) would be equivalent to the EBNF
+    a = [b];
+  The new rule's product would be either a-product, if a accepts it, or else nil. Note
+  that the latter actually means that the new rule would then return the vector
+  [nil tokens]. The new rule can never simply return nil."
+  [subrule]
+  (fn [tokens]
+      (or (subrule tokens) [nil tokens])))
+
+(defn rep*
+  "Creates a rule function that is the zero-or-more repetition of the given subrule--that
+  is, either zero or more of the subrule.
+  (def a (rep* b)) would be equivalent to the EBNF
+    a = {b};
+  The new rule's products would be either the vector [b-product ...] for how many matches
+  of b were found, or the empty vector [] if there was no match. Note that the latter
+  actually means that the new rule would then return the vector [[] tokens]. The new rule
+  can never simply return nil."
+  [subrule]
+  (fn [tokens]
+      (loop [products [], token-queue tokens]
+            (let [cur-result (subrule token-queue)]
+                 (if (or (nil? cur-result) (= cur-result [nil nil])),
+                     [products token-queue],
+                     (recur (conj products (cur-result 0))
+                            (cur-result 1)))))))
+
+(defn rep+
+  "Creates a rule function that is the one-or-more repetition of the given rule--that
+  is, either one or more of the rule.
+  (def a (rep+ b)) would be equivalent to the EBNF
+    a = {b}-;
+  The new rule's products would be the vector [b-product ...] for how many matches of b
+  were found. If there were zero matches, then nil is simply returned."
+  [subrule]
+  (fn [tokens]
+      (let [product ((rep* subrule) tokens)]
+           (if (not (empty? (product 0))), product))))
+
+(defn lit-seq
+  "Creates a rule function that is the concatenation of the literals of the sequence of the
+  given sequenceable object--that is, it accepts only a series of tokens that matches the
+  sequence of the token sequence.
+  (def a (lit-seq \"ABCD\")) would be equivalent to the EBNF
+    a = \"A\", \"B\", \"C\", \"D\";
+  The new rule's products would be the result of the concatenation rule."
+  [token-seq]
+  (semantics (apply conc (map lit token-seq)) seq))
+
+(defn emptiness
+  "A rule function that matches emptiness--that is, it always matches with every given token
+  sequence, and it always returns [nil tokens]."
+  [tokens]
+  [nil tokens])
+
+(defn flatten
+  "Takes any nested combination of sequential things (lists, vectors,
+  etc.) and returns their contents as a single, flat sequence."
+  [x]
+  (let [s? #(instance? clojure.lang.Sequential %)]
+    (filter (complement s?) (tree-seq s? seq x))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/name/choi/joshua/fnparse/json.clj
@@ -0,0 +1,162 @@
+(ns name.choi.joshua.fnparse.json
+  (:refer-clojure)
+  (:use clojure.parse))
+
+(defstruct node :kind :content)
+
+(def string-delimiter (lit \"))
+(def escape-indicator (lit \\))
+(def false-lit (constant-semantics (lit-seq "false") (struct node :scalar false)))
+(def true-lit (constant-semantics (lit-seq "true") (struct node :scalar true)))
+(def null-lit (constant-semantics (lit-seq "null") (struct node :scalar nil)))
+(def keyword-lit (alt false-lit true-lit null-lit))
+
+(def ws (rep* (apply alt (map lit [\space \tab \newline \return]))))
+
+(def begin-array (constant-semantics (conc ws (lit \[) ws) :begin-array))
+(def end-array (constant-semantics (conc ws (lit \]) ws) :end-array))
+(def begin-object (constant-semantics (conc ws (lit \{) ws) :begin-object))
+(def end-object (constant-semantics (conc ws (lit \}) ws) :end-object))
+(def name-separator (constant-semantics (conc ws (lit \:) ws) :name-separator))
+(def value-separator (constant-semantics (conc ws (lit \,) ws) :value-separator))
+
+(def minus-sign (lit \-))
+(def plus-sign (lit \+))
+(def decimal-point (lit \.))
+(def exponential-sign (alt (lit \e) (lit \E)))
+(def zero-digit (lit \0))
+(def nonzero-decimal-digit
+  (alt (lit \1) (lit \2) (lit \3) (lit \4) (lit \5) (lit \6) (lit \7)
+       (lit \8) (lit \9)))
+(def decimal-digit (alt zero-digit nonzero-decimal-digit))
+(def fractional-part (conc decimal-point (rep* decimal-digit)))
+(def exponential-part
+  (conc exponential-sign (opt (alt plus-sign minus-sign)) (rep+ decimal-digit)))
+
+(def number-lit
+  (semantics
+    (conc (opt minus-sign) (alt zero-digit (rep+ nonzero-decimal-digit))
+          (opt fractional-part) (opt exponential-part))
+    (fn [product]
+        (if (or (product 2) (product 3))
+            (hash-map :kind :scalar, :content (Double/parseDouble
+                                      (apply str (flatten product))))
+            (hash-map :kind :scalar, :content (Integer/parseInt
+                                        (apply str (flatten product))))))))
+
+(def hexadecimal-digit
+  (alt decimal-digit (lit \A) (lit \B) (lit \C) (lit \D) (lit \E) (lit \F)))
+
+(def unescaped-char (term #(and (not= % \\) (not= % \"))))
+
+(def unicode-char-sequence
+  (semantics (conc (lit \u) hexadecimal-digit
+                   hexadecimal-digit hexadecimal-digit hexadecimal-digit)
+             #(char (Integer/parseInt (apply str (rest %)) 16))))
+
+(defn escape-sequence-rule
+  "Creates a rule for escape sequences--that is, the escape indicator followed by
+  the given escaped character, together turning into the given new character. The
+  new character is the same given escaped character by default."
+  ([escaped-character]
+   (escape-sequence-rule escaped-character escaped-character))
+  ([escaped-character new-character]
+   (semantics (lit escaped-character)
+              (fn [_] new-character))))
+
+(def escape-sequence
+  (semantics
+    (conc escape-indicator
+          (alt (escape-sequence-rule \")
+               (escape-sequence-rule \\)
+               (escape-sequence-rule \/)
+               (escape-sequence-rule \b \backspace)
+               (escape-sequence-rule \f \formfeed)
+               (escape-sequence-rule \n \newline)
+               (escape-sequence-rule \r \return)
+               (escape-sequence-rule \t \tab)
+               (escape-sequence-rule \\)
+               unicode-char-sequence))
+    #(% 1)))
+
+(def string-char
+  (alt unescaped-char escape-sequence))
+
+(def string-lit
+  (semantics (conc string-delimiter (rep* string-char) string-delimiter)
+             #(hash-map :kind :scalar, :content (apply str (% 1)))))
+
+(declare array)
+(declare object)
+
+(defn value [tokens]
+  (some #(% tokens) [string-lit number-lit keyword-lit array object]))
+
+(def additional-value
+  (semantics (conc value-separator value) #(% 1)))
+
+(def array-contents
+  (semantics (conc value (rep* additional-value))
+             #(into [(% 0)] (% 1))))
+
+(def array
+  (semantics (conc begin-array (opt array-contents) end-array)
+             #(hash-map :kind :array, :content (vec (% 1)))))
+
+(def entry
+  (semantics (conc string-lit name-separator value)
+             #(vector (% 0) (% 2))))
+
+(def additional-entry
+  (semantics (conc value-separator entry) #(% 1)))
+
+(def object-contents
+  (semantics (conc entry (rep* additional-entry))
+             #(into [(% 0)] (% 1))))
+
+(def object
+  (semantics (conc begin-object (opt object-contents) end-object)
+             #(hash-map :kind :object, :content (into {} (% 1)))))
+
+(def text (alt object array))
+
+(def lex seq)
+
+(defn parse [tokens]
+  (let [[product remainder] (text tokens)]
+    product))
+
+(defmulti represent :kind)
+
+(defmethod represent :object [node]
+  (into {} (map #(vector (represent (key %)) (represent (val %))) (:content node))))
+
+(defmethod represent :array [node]
+  (vec (map #(represent %) (:content node))))
+
+(defmethod represent :scalar [node]
+  (:content node))
+
+(def load-stream (comp represent parse lex))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
