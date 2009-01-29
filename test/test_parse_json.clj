00e944873f3c652447447bbba8750d7568c2c3cd
joshua-choi <joshua@choi.name>
2009-01-29 10:26:07 -0700

    Commented all tests. 	modified:   test/test_parse_json.clj

--- test/test_parse_json.clj
+++ test/test_parse_json.clj
@@ -203,14 +203,14 @@
 ;          (seq ",")])
 ;      "parsing a padded size-3 object"))
 ;
-(deftest test-load-stream
-  (is (= (j/load-stream "[1, 2, 3]") [1 2 3])
-      "loading a flat vector containing integers")
-  (is (= (j/load-stream "[\"a\", \"b\\n\", \"\\u1234\"]")
-         ["a" "b\n" "\u1234"])
-      "loading a flat vector containing strings")
-  (is (= (j/load-stream "{\"a\": 1, \"b\\n\": 2, \"\\u1234\": 3}")
-         {"a" 1, "b\n" 2, "\u1234" 3})
-      "loading a flat object containing strings and integers"))
+;(deftest test-load-stream
+;  (is (= (j/load-stream "[1, 2, 3]") [1 2 3])
+;      "loading a flat vector containing integers")
+;  (is (= (j/load-stream "[\"a\", \"b\\n\", \"\\u1234\"]")
+;         ["a" "b\n" "\u1234"])
+;      "loading a flat vector containing strings")
+;  (is (= (j/load-stream "{\"a\": 1, \"b\\n\": 2, \"\\u1234\": 3}")
+;         {"a" 1, "b\n" 2, "\u1234" 3})
+;      "loading a flat object containing strings and integers"))
 
 (time (run-tests))
