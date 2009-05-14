b41d2c423beb7393c624afdc3cbee24c1a008be4
joshua-choi <joshua@choi.name>
2009-05-14 13:32:07 -0700

    added test for load-stream

--- test/test_parse_json.clj
+++ test/test_parse_json.clj
@@ -33,14 +33,14 @@
   (is (= (j/string-lit (make-state "\"hello\\u1111\"]" 3 4))
          [(make-node :scalar "hello\u1111") (make-state (seq "]") 16 4)])))
 
-;(deftest load-stream
-;  (is (= (j/load-stream "[1, 2, 3]") [1 2 3])
-;      "loading a flat vector containing integers")
-;  (is (= (j/load-stream "[\"a\", \"b\\n\", \"\\u1234\"]")
-;         ["a" "b\n" "\u1234"])
-;      "loading a flat vector containing strings")
-;  (is (= (j/load-stream "{\"a\": 1, \"b\\n\": 2, \"\\u1234\": 3)")
-;         {"a" 1, "b\n" 2, "\u1234" 3))
-;      "loading a flat object containing strings and integers"))
+(deftest load-stream
+  (is (= (j/load-stream "[1, 2, 3]") [1 2 3])
+      "loading a flat vector containing integers")
+  (is (= (j/load-stream "[\"a\", \"b\\n\", \"\\u1234\"]")
+         ["a" "b\n" "\u1234"])
+      "loading a flat vector containing strings")
+  (is (= (j/load-stream "{\"a\": 1, \"b\\n\": 2, \"\\u1234\": 3")
+         {"a" 1, "b\n" 2, "\u1234" 3})
+      "loading a flat object containing strings and integers"))
 
 (time (run-tests))
