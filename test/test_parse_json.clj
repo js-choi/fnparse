69099c1a4d1a53f0ac48d201872c6608abd3d533
joshua-choi <joshua@choi.name>
2009-05-18 16:44:43 -0700

    added test for [1, 2, 3}

--- test/test_parse_json.clj
+++ test/test_parse_json.clj
@@ -60,10 +60,14 @@
 
 (deftest load-stream
   (is (= (j/load-stream "[1, 2, 3]") [1 2 3])
-      "loading a flat vector containing integers")
+      "loading a flat array containing integers")
+  (is (thrown-with-msg? Exception
+        #"an array is unclosed; \"]\" expected where \"}\" is"
+        (j/load-stream "[1, 2, 3}") [1 2 3])
+      "loading an improperly closed array")
   (is (= (j/load-stream "[\"a\", \"b\\n\", \"\\u1234\"]")
          ["a" "b\n" "\u1234"])
-      "loading a flat vector containing strings")
+      "loading a flat array containing strings")
   (is (= (j/load-stream "{\"a\": 1, \"b\\n\": 2, \"\\u1234\": 3}")
          {"a" 1, "b\n" 2, "\u1234" 3})
       "loading a flat object containing strings and integers"))
