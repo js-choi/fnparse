95255b428a9aec93ba0f8e1e9b09a163fd2649a9
joshua-choi <joshua@choi.name>
2009-12-24 11:19:03 -0700

    edited project.clj: thanks to neotyk for the patch

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fnparse "2.2.4"
   :description "A library for creating functional parsers in Clojure."
-  :dependencies [[org.clojure/clojure "1.0.0"]
-                 [org.clojure/clojure "1.0.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"
+                 [org.clojure/clojure-contrib "1.0.0-master-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "1.0.0"]])
 
