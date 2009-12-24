746ac8a884608e03a258c133f17a087533e63b9a
joshua-choi <joshua@choi.name>
2009-12-23 21:21:04 -0700

    created an untested, preliminary Leiningen project file

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject fnparse "2.2.4"
+  :description "A library for creating functional parsers in Clojure."
+  :dependencies [[org.clojure/clojure "1.0.0"]
+                 [org.clojure/clojure "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "1.0.0"]])
+
