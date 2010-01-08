e2985ab3700f7098032cadd24416db722585d07d
joshua-choi <joshua@choi.name>
2010-01-08 15:08:51 -0700

    clojure.clj: changed defs and defns to devar-s and defn-s

--- src/name/choi/joshua/fnparse/clojure.clj
+++ src/name/choi/joshua/fnparse/clojure.clj
@@ -11,30 +11,30 @@
 
 (declare form)
 
-(def ws-set (set " ,\t\n"))
-(def indicator-set (set ";()[]{}\\\"'@^`#"))
-(def separator-set (union ws-set indicator-set))
-(def comment-r (conc (lit \;) (rep* (antilit \newline))))
-(def discarded-form (prefix-conc (lex (mapconc "#_")) #'form))
-(def ws
-  (with-label "ws"
+(defvar- ws-set (set " ,\t\n"))
+(defvar- indicator-set (set ";()[]{}\\\"'@^`#"))
+(defvar- separator-set (union ws-set indicator-set))
+(defvar- comment-r (conc (lit \;) (rep* (antilit \newline))))
+(defvar- discarded-form (prefix-conc (lex (mapconc "#_")) #'form))
+(defvar- ws
+  (with-label "whitespace"
     (rep+ (alt (term "whitespace character" ws-set)
-               comment-r discarded-form)))
-(def ws? (opt ws))
-(def indicator (term "indicator" indicator-set))
-(def symbol-char (antiterm "symbol char" separator-set))
-(def form-end (alt (followed-by (alt ws indicator)) end-of-input))
+               comment-r discarded-form))))
+(defvar- ws? (opt ws))
+(defvar- indicator (term "indicator" indicator-set))
+(defvar- symbol-char (antiterm "symbol char" separator-set))
+(defvar- form-end (alt (followed-by (alt ws indicator)) end-of-input))
 
-(def symbol-r
+(defvar- symbol-r
   (complex [first-letter ascii-letter, other-chars (rep* symbol-char)]
     (->> other-chars (cons first-letter) (apply str) symbol)))
 
-(def division-symbol (constant-semantics (lit \/) '/))
+(defvar- division-symbol (constant-semantics (lit \/) '/))
 
 (let [decimal-digits (rep+ decimal-digit)
       optional-sign (opt (set-lit "plus or minus sign" "+-"))
       digits-to-int #(->> % (apply str) Integer/parseInt)]
-  (def decimal-number
+  (defvar- decimal-number
     (complex [sign optional-sign
               body decimal-digits
               tail (alt (opt (conc (lit \/) decimal-digits))
@@ -51,24 +51,23 @@
             (->> tail (concat signed-body) (apply str)
               Double/parseDouble)))))))
 
-(def string-delimiter (lit \"))
-(def escape-sequence-map
+(defvar- string-delimiter (lit \"))
+(defvar- escape-sequence-map
   {\t \tab
    \n \newline
    \\ \\
    \" \"})
-(def escape-sequence
+(defvar- escape-sequence
   (semantics (prefix-conc (lit \\) (set-lit "valid escape sequence"
                                      (keys escape-sequence-map)))
     escape-sequence-map))
-(def string-r
-  (semantics (circumfix-conc string-delimiter
-                             (rep* (alt escape-sequence (antilit \")))
-                             string-delimiter)
+(defvar- string-r
+  (semantics (suffix-conc (rep* (alt escape-sequence (antilit \")))
+                          string-delimiter)
     #(->> % flatten (apply str))))
 
 (do-template [rule-name prefix-char product-fn-symbol]
-  (def rule-name
+  (defvar- rule-name
     (complex [_ (lit prefix-char), content #'form]
       (list product-fn-symbol content)))
   quoted-form \' `quote
@@ -77,32 +76,32 @@
   derefed-form \@ `deref
   var-inner-r \' `var)
 
-(def unquote-spliced-form
+(defvar- unquote-spliced-form
   (semantics (prefix-conc (mapconc "~@") #'form)
     #(list `unquote-splicing %)))
 
-(def character-name
+(defvar- character-name
   (mapalt #(constant-semantics (mapconc (val %)) (key %))
     char-name-string))
 
-(def character-r
+(defvar- character-r
   (complex [_ (lit \\), content character-name]
     content))
 
-(def special-symbol
+(defvar- special-symbol
   (suffix-conc
     (mapalt #(constant-semantics (mapconc (key %)) (val %))
       {"nil" nil, "true" :true, "false" false})
     form-end))
 
-(def keyword-r
+(defvar- keyword-r
   (complex [_ (lit \:), content symbol-r]
     content))
 
-(def form-series (suffix-conc (rep* #'form) ws?))
+(defvar- form-series (suffix-conc (rep* #'form) ws?))
 
 (do-template [rule-name start-token end-token product-fn]
-  (def rule-name
+  (defvar- rule-name
     (complex [_ (lit start-token)
               contents (opt form-series)
               _ (with-label (format "a %s or an form" end-token)
@@ -113,12 +112,12 @@
   map-r \{ \} #(apply hash-map %)
   set-inner-r \{ \} set)
 
-(def dispatched-form
+(defvar- dispatched-form
   (prefix-conc
     (lit \#)
     (template-alt [label prefix-token body]
       (with-label label (prefix-conc (lit prefix-token) body))
-      "a set" \{
+      "a set form" \{
         (semantics (suffix-conc form-series (lit \})) set)
       "a mini-function" \(
         (semantics (suffix-conc form-series (lit \))) #(list `mini-fn %))
@@ -130,15 +129,39 @@
                   base-form #'form]
           (list `with-meta base-form metadata)))))
 
-(def form
+(defn- add-head [f]
+  #(list f %))
+
+(defn- head-prefixed-form [f body]
+  (semantics body (add-head f)))
+
+(defn- delimited-form-series [end-token]
+  (suffix-conc form-series (with-label (format "a %s or form" end-token)
+                             (lit end-token))))
+
+(defvar- reader-macro
+  (template-alt [label prefix-token body]
+    (with-label label (prefix-conc (lit prefix-token) body))
+    "quoted form" \' (head-prefixed-form `quote #'form)
+    "derefed form" \@ (head-prefixed-form `deref #'form)
+    "syntax-quoted form" \` (head-prefixed-form `syntax-quote #'form)
+    "unquoted form" \~ (head-prefixed-form `unquote #'form)
+    "list form" \( (semantics (delimited-form-series \)) list*)
+    "vector form" \[ (semantics (delimited-form-series \]) vec)
+    "map form" \{ (semantics (delimited-form-series \}) #(apply hash-map %))))
+
+(defvar- form
   (with-label "a form"
     (prefix-conc
       ws?
-      (alt list-r vector-r map-r string-r dispatched-form quoted-form syntax-quoted-form (lex unquote-spliced-form) unquoted-form derefed-form division-symbol character-r keyword-r (lex special-symbol) symbol-r decimal-number))))
+      (alt dispatched-form reader-macro syntax-quoted-form (lex unquote-spliced-form) unquoted-form division-symbol character-r keyword-r (lex special-symbol) symbol-r decimal-number))))
+;       (alt string-r dispatched-form quoted-form syntax-quoted-form (lex unquote-spliced-form) unquoted-form derefed-form division-symbol character-r keyword-r (lex special-symbol) symbol-r decimal-number))))
 
 ; (-> "#^{} #{[a b;Comment\nc]}" make-state form prn)
-(-> "#_#_'a'b'c" make-state form prn)
+; (-> "#_#_'a'b'c" make-state form prn)
+(-> "{a b c d}" make-state form prn)
 ; (-> "aa\" 2\"]" make-state form println)
 ; (-> "\"a\\tb\"" make-state form prn)
 ; (-> "\\t\"" make-state escape-sequence prn)
 ; (-> "a b;Comment\nc" make-state form-series prn)
+; (-> "'a b;Comment\nc" make-state reader-macro prn)
