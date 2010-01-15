46cc340a1935baa74340dbd9515c4a23e1586726
joshua-choi <joshua@choi.name>
2010-01-15 15:46:15 -0700

    corrected further project.clj, based on hiredman's suggestions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject fnparse "2.2.4"
+(defproject fnparse "2.2.5"
   :description "A library for creating functional parsers in Clojure."
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0.0-master-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "1.0.0"]])
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]])
 
