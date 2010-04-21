0792612f92dfc90ad143ec0f9a3e862585c3f420
joshua-choi <joshua.sunchul.choi@gmail.com>
2010-04-20 19:13:15 -0700

    tweaked documentation in json.clj

--- src/edu/arizona/fnparse/json.clj
+++ src/edu/arizona/fnparse/json.clj
@@ -21,7 +21,7 @@
   (apply str objects))
 
 (c/defrule <ws?>
-  "Optional, ignored JSON whitespace."
+  "Consumes optional, ignored JSON whitespace."
   (p/rep* (p/set-term "whitespace" " \t\n\r")))
 
 ; Define single-character indicator rules.
@@ -50,11 +50,12 @@
   <null>  "null"  nil)
 
 (c/defrule <control-char>
-  "An ASCII control character, which is not allowed in strings."
+  "Consumes an ASCII control character, which is not allowed in strings."
   (p/chook ::control-char p/<ascii-control>))
 
 (c/defrule <normal-str-char>
-  "A normal, non-espaced string character. No control characters allowed."
+  "Consumes normal, non-espaced string character.
+  No control characters allowed."
   {:product "A character."}
   (p/except "a normal string character"
     (fn [subtrahend-prod]
@@ -71,7 +72,7 @@
   (reduce #(+ (* 16 %1) %2) digits))
 
 (c/defrule <unicode-sequence>
-  "The lowercase u followed by hexadecimal digits."
+  "Consumes a lowercase 'u' followed by hexadecimal digits."
   {:product "The character with the given digits' Unicode code."}
   (p/prefix
     (p/lit \u)
@@ -79,7 +80,7 @@
       (p/factor= 4 p/<hexadecimal-digit>))))
 
 (c/defrule <escaped-str-char>
-  "An escaped character in a string: a backslash
+  "Consumes an escaped character in a string: a backslash
   followed by an escape sequence."
   {:product "A character."}
   (p/prefix
@@ -88,18 +89,18 @@
          <unicode-sequence>)))
 
 (c/defrule <str-char>
-  "A general string character."
+  "Consumes a general string character."
   {:product "A character or a sequence of characters."
    :consumes "One character."}
   (p/+ <escaped-str-char> <normal-str-char>))
 
 (c/defrule <str-content>
-  "A sequence of string characters."
+  "Consumes a sequence of string characters."
   {:product "A string.", :consumes "Zero or more characters."}
   (p/hook (comp str* concat) (p/rep* (p/+ <str-char>))))
 
 (c/defrule <str>
-  "A JSON string."
+  "Consumes a JSON string."
   {:product "A string."}
   (p/label "a string"
     (p/circumfix <str-delimiter> <str-content> <str-delimiter>)))
@@ -110,39 +111,41 @@
 (declare <value>)
 
 (c/defrule <array-content>
-  "A sequence of JSON values separated by commas, with optional
+  "Consumes a sequence of JSON values separated by commas, with optional
   whitespace padding on the front."
   {:product "A vector."}
   (p/separated-rep* <value-separator> #'<value>))
 
 (c/defrule <array>
-  "A JSON array. Optionally padded on the front with whitespace."
+  "Consumes a JSON array. Optionally padded on the front with whitespace."
   {:product "A vector."}
   (p/circumfix <array-start> <array-content> <array-end>))
 
 (c/defrule <object-entry>
-  "A string-value pair with a colon. They appear in objects.
+  "Consumes a string-value pair with a colon. They appear in objects.
   Optionally padded on the front with whitespace."
   {:product "A vector pair."}
   (p/for [name <str>, _ <name-separator>, val #'<value>]
     [name val]))
 
 (c/defrule <object-content>
-  "A sequence of object entries separated by commas. Optionally
+  "Consumes a sequence of object entries separated by commas. Optionally
   padded with whitepace on the front."
   (p/hook #(into {} %) (p/separated-rep* <value-separator> <object-entry>)))
 
 (c/defrule <object>
-  "A JSON object. Optionally padded on the front with whitespace."
+  "Consumes a JSON object. Optionally padded on the front with whitespace."
   {:product "A map."}
   (p/circumfix <object-start> <object-content> <object-end>))
 
 (c/defrule <value>
-  "A general JSON value, optionally padded with whitespace on the front."
+  "Consumes a general JSON value, optionally padded
+  with whitespace on the front."
   {:product "A vector, map, number, true, false, or nil."}
   (p/prefix <ws?> (p/+ <object> <array> <str> <true> <false> <null>)))
 
 (c/defrule <document>
-  "A general JSON document, optionally padded with whitespace on both sides.
+  "Consumes a general JSON document, optionally padded
+  with whitespace on both sides.
   The root rule of the JSON grammar."
   (p/suffix <value> <ws?>))
