930f0dc6e7b6ed1cade2de9d90e7f924a4bec17b
joshua-choi <joshua.sunchul.choi@gmail.com>
2010-06-10 10:49:42 -0700

    deleted clojure-pure (too much of a hassle) and cat/test and hound/test

--- src/edu/arizona/fnparse/cat/test.clj
+++
@@ -1,5 +0,0 @@
-(ns edu.arizona.fnparse.cat.test
-  (:require [edu.arizona.fnparse :as c]
-            [edu.arizona.fnparse.cat :as p]
-            [clojure.test :as test]))
-
--- src/edu/arizona/fnparse/hound/test.clj
+++
@@ -1,5 +0,0 @@
-(ns edu.arizona.fnparse.hound.test
-  (:require [edu.arizona.fnparse :as c]
-            [edu.arizona.fnparse.hound :as p]
-            [clojure.test :as test]))
-
