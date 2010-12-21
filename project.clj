fa6a8216c3f43d0d26d5f9b36099c1f5d9fae1fc
joshua-choi <joshua.sunchul.choi@gmail.com>
2010-12-20 17:27:04 -0700

    Modified project version to 3 point alpha point 4, 3.α.4.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject fnparse "3.0.0.prealpha1"
+(defproject fnparse "3.α.4"
   :description "A library for creating functional parsers in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]])
