11a7b00e6e0077997335adedc5bd00a4a4a87ddc
joshua-choi <joshua.sunchul.choi@gmail.com>
2010-12-20 17:28:15 -0700

    Removed extra project file line header.

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject fnparse "3.0.0.prealpha1"
 (defproject fnparse "3.α.4"
   :description "A library for creating functional parsers in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
