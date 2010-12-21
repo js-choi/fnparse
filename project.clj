3a1a9578abaa4f5b7f40921db9e6ade3655d2654
joshua-choi <joshua.sunchul.choi@gmail.com>
2010-12-20 17:16:21 -0700

    for some reason, the merge did not modify project.clj. Changed Clojure versions.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fnparse "2.2.7"
   :description "A library for creating functional parsers in Clojure."
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]])
 
