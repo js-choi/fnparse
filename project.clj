6bc67ed73194f89df28b1e194f6ab1ed7f232062
joshua-choi <joshua.sunchul.choi@gmail.com>
2010-12-20 17:27:42 -0700

    Added dev. dependencies to project.clj for Clojars.

--- project.clj
+++ project.clj
@@ -3,3 +3,5 @@
   :description "A library for creating functional parsers in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]]
+  :repositories [["clojars" "http://clojars.org/repo"]])
